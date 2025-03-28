8ba1b98602fb0cfcf1d7f26f667c8f989a110744
desivar <jillian_desire@yahoo.es>
2025-03-28 06:52:39 -0600

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+// const contactsRoutes = require('./contacts');
+// router.use('/', contactsRoutes);
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World from routes!');
 });
 
 module.exports = router;
