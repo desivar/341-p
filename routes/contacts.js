6e4bfcc0c3a0b39d79bb70140dc185d1bcdbe8d8
desivar <jillian_desire@yahoo.es>
2025-03-28 14:28:00 -0600

    cahnge in path

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const contactsController = require('../../controllers/contacts');
+const contactsController = require('../controllers/contacts'); // Changed the path
 
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getById);
