7a2de55f0ed684258cce56c39c42be6bd2f9c0cb
desivar <jillian_desire@yahoo.es>
2025-03-28 14:22:31 -0600

    add the routes/contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const contactsController = require('../controllers/contacts');
+const contactsController = require('../../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getById);
+router.post('/', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
