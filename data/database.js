2bd6f518fb428c35da0763deb635dbf32363dce5
desivar <jillian_desire@yahoo.es>
2025-03-28 05:58:09 -0600

    typo corrected

--- data/database.js
+++ data/database.js
@@ -1,20 +1,32 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const MongoClient=require('mongo').MongoClient;
+const { MongoClient } = require('mongodb'); // Corrected import
 
 let database;
-const intDb= (callback) =>{
-    if (database){
-        console.log('Database is already initialized!');
-        return callback(null,database);
-    }
-    MongoClient.connect(process.env.MONGODB_URL)
+const initDb = (callback) => { // Corrected function name
+  if (database) {
+    console.log('Database is already initialized!');
+    return callback(null, database);
+  }
+  MongoClient.connect(process.env.MONGODB_URL)
     .then(client => {
-        database=client;
-        callback(null,database);
+      database = client.db(); // It's generally better to get the database instance here
+      callback(null, database);
     })
     .catch(err => {
-        callback(err);
-        });
-    };
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!database) {
+    throw Error('Db not initialized');
+  }
+  return database;
+};
+
+module.exports = {
+  initDb,
+  getDb,
+};
