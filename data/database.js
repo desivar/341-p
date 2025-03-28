0eeb3d67da70755c5e8d483875664b1ba5ed04f4
desivar <jillian_desire@yahoo.es>
2025-03-28 05:45:14 -0600

    database

--- data/database.js
+++ data/database.js
@@ -1,29 +1,20 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const dotenv=require('dotenv');
+dotenv.config();
+
+const MongoClient=require('mongo').MongoClient;
+
 let database;
-const initDb = (callback) => {
-    if (database) {
-        console.log('Db is already initialized');
-        return callback(null, database);
+const intDb= (callback) =>{
+    if (database){
+        console.log('Database is already initialized!');
+        return callback(null,database);
     }
     MongoClient.connect(process.env.MONGODB_URL)
-        .then((client) => {
-            database = client;
-            callback(null, database);
-        })
-        .catch((err) => {
-            callback(err);
+    .then(client => {
+        database=client;
+        callback(null,database);
+    })
+    .catch(err => {
+        callback(err);
         });
-};
-
-const getDb = () => {
-    if (!database) {
-        throw Error('No database found!');
-    }
-    return database;
-};
-
-module.exports = {
-    initDb,
-    getDb,
-};
+    };
