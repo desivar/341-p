ce67699ff4c171960a27a6afc642b75a65796df6
desivar <jillian_desire@yahoo.es>
2025-03-28 14:34:27 -0600

    add router

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const contactsRoutes = require('./contacts');
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the API!'); // Or some other message
+  res.send('Welcome to the Contacts API!'); // Or a more descriptive message
 });
 
 router.use('/contacts', contactsRoutes);
