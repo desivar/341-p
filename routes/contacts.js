179f4c099b51cc5cafc0555cbc7459cf2abb11bf
desivar <jillian_desire@yahoo.es>
2025-03-28 08:09:10 -0600

    fix contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-console.log('Current working directory in routes/contacts.js:', process.cwd());
 const contactsController = require('../../controllers/contacts');
 
 router.get('/', contactsController.getAll); // This will handle GET /contacts
