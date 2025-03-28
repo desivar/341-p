ccae07977578db13084ba2435aad12046b80d6df
desivar <jillian_desire@yahoo.es>
2025-03-28 06:45:51 -0600

    add parser

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
-const mongodb = require('./data/database');
-const ObjectId = require('mongodb').ObjectId;
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mongodb = require('./data/databaseb');
+const routes = require('./routes');
 
-const getAll = async (req, res) => {
-  try {
-    const db = mongodb.getDb();
-    const contacts = await db.collection('contacts').find().toArray();
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(contacts);
-  } catch (error) {
-    console.error('Error getting all contacts:', error);
-    res.status(500).json({ message: 'Failed to retrieve contacts' });
+const app = express();
+const port = process.env.PORT || 5500;
+
+app.use(bodyParser.json()); // For parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+app.use('/', routes);
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on port http://0.0.0.0:${port}`);
+    });
   }
-};
-
-const getById = async (req, res) => {
-  const contactId = new ObjectId(req.params.id);
-  try {
-    const db = mongodb.getDb();
-    const contact = await db.collection('contacts').findOne({ _id: contactId });
-    if (contact) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(contact);
-    } else {
-      res.status(404).json({ message: 'Contact not found' });
-    }
-  } catch (error) {
-    console.error(`Error getting contact with ID ${req.params.id}:`, error);
-    res.status(500).json({ message: 'Failed to retrieve contact' });
-  }
-};
-
-module.exports = { getAll, getById };
+});
